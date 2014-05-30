8d21f2cad4d220953a15d71e7337f9abdd62e642
radko26 <radipirate@gmail.com>
2014-05-30 17:22:08 +0300

    update
    New solution added

--- src/main/java/com/sirma/itt/javacourse/objects/Sumator/Main.java
+++ src/main/java/com/sirma/itt/javacourse/objects/Sumator/Main.java
@@ -3,27 +3,22 @@ package main.java.com.sirma.itt.javacourse.objects.Sumator;
 /**
  * Sumator problem main class
  * 
- * 
  * @author Radoslav
  */
 public class Main {
 
 	private static Sumator sumator;
-	
+
 	public static void main(String[] args) {
 		/**
 		 * @link to the sumator class
 		 */
 		sumator = new Sumator();
-		String a ="-5650";
+		String a = "-5650";
 		String b = "1";
-		
-		System.out.println(sumator.sum(a,b));
-		
-		
-		
-		
-		
+
+		System.out.println(sumator.sum(a, b));
+
 	}
 
 }
--- src/main/java/com/sirma/itt/javacourse/objects/Sumator/Sumator.java
+++ src/main/java/com/sirma/itt/javacourse/objects/Sumator/Sumator.java
@@ -4,10 +4,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 /**
- * Sumator Class:
- * For given 2 numbers it calculates their sum
- * 
- * sum(a,b) where a,b can be Integer,Float,String,BigInteger,BigDecimal
+ * Sumator Class: For given 2 numbers it calculates their sum
  * 
  * @author Radoslav
  */
@@ -15,75 +12,77 @@ import java.math.BigInteger;
 public class Sumator {
 	private BigInteger sumBigInteger;
 	private BigDecimal sumBigDecimal;
-	
+
 	/**
-	 * via the Sumator() constructor
-	 * the initialization of variables is done
+	 * via the Sumator() constructor the initialization of variables is done
 	 * 
 	 */
-	public Sumator(){
-		sumBigInteger= new BigInteger("0");
-		sumBigDecimal= new BigDecimal(0);
+	public Sumator() {
+		sumBigInteger = new BigInteger("0");
+		sumBigDecimal = new BigDecimal(0);
 	}
+
 	/**
-	 *
+	 * 
 	 * @param a
-	 * 		numberA (int)
+	 *            numberA (int)
 	 * @param b
-	 * 		numberB (int)
+	 *            numberB (int)
 	 * @return their sum (int)
 	 */
-	public int sum(int a,int b){
-		return a+b;
+	public int sum(int a, int b) {
+		return a + b;
 	}
-	//same as the int type
-	public float sum(float a,float b){
-		return a+b;
+
+	// same as the int type
+	public float sum(float a, float b) {
+		return a + b;
 	}
-	
+
 	/**
-	 * Making a new BigInteger object
-	 * and add both @param a , @param b to it
+	 * Making a new BigInteger object and add both @param a , @param b to it
 	 * 
 	 * 
 	 * @param a
-	 * 		BigInteger object
+	 *            BigInteger object
 	 * @param b
-	 * 		BigInteger object
-	 * @return String representation of the new BigInteger object in which we kept the sum
+	 *            BigInteger object
+	 * @return String representation of the new BigInteger object in which we
+	 *         keep the sum
 	 */
-	public String sum(BigInteger a,BigInteger b){
-		sumBigInteger=sumBigInteger.add(a);
-		sumBigInteger=sumBigInteger.add(b);
+	public String sum(BigInteger a, BigInteger b) {
+		sumBigInteger = sumBigInteger.add(a);
+		sumBigInteger = sumBigInteger.add(b);
 		return sumBigInteger.toString();
 	}
+
 	// same as BigInteger type
 	// only the @param a,b are BigDecimal
 	// and we store the sum in BigDecimal object
-	public String sum(BigDecimal a,BigDecimal b){
-		sumBigDecimal=sumBigDecimal.add(a);
-		sumBigDecimal=sumBigDecimal.add(b);
+	public String sum(BigDecimal a, BigDecimal b) {
+		sumBigDecimal = sumBigDecimal.add(a);
+		sumBigDecimal = sumBigDecimal.add(b);
 		return sumBigDecimal.toString();
 	}
+
 	/**
-	 * Summing 2 string numbers, requires own implementation or 
-	 * making a BigInteger object with the string's value
-	 * Here we use the build in java.math.BigInteger
+	 * Summing 2 string numbers, requires own implementation or making a
+	 * BigInteger object with the string's value. Here we use the build in
+	 * java.math.BigInteger
 	 * 
 	 * @param a
-	 * 		number is passed in String
+	 *            number is passed in a string
 	 * @param b
-	 * 		number is passed in String
+	 *            number is passed in a string
 	 * @return the String representation of the BigInteger object
 	 */
-	public String sum(String a,String b){
+	public String sum(String a, String b) {
 		BigInteger numA = new BigInteger(a);
 		BigInteger numB = new BigInteger(b);
-		
+
 		sumBigInteger = sumBigInteger.add(numA);
 		sumBigInteger = sumBigInteger.add(numB);
 		return sumBigInteger.toString();
 	}
-	
-	
+
 }
